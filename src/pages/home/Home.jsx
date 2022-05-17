b67b8e2b8c6a7818f6e8257b75ad707d24fda239
Hennization <shy6295@ewhain.net>
2022-05-17 19:26:35 +0900

    feat: moddleBanner 구현

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import MiddleBanner from "components/home/MiddleBanner";
+
 function Home() {
-  return <StyledRoot>Home</StyledRoot>;
+  return (
+    <StyledRoot>
+      <MiddleBanner />
+    </StyledRoot>
+  );
 }
 
 export default Home;
 
-const StyledRoot = styled.div``;
+const StyledRoot = styled.div`
+  width: 172.8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
