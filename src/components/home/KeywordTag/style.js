c9894293a4277ae8a739d58c647c664a2faa2543
choichoijin <cuj10594@naver.com>
2022-05-19 03:59:20 +0900

    chore: 코드 정리

--- src/components/home/KeywordTag/style.js
+++ src/components/home/KeywordTag/style.js
@@ -6,36 +6,39 @@ export const StyledRoot = styled.div`
   h3 {
     font-family: "AppleSDGothicNeoB";
     src: url("./AppleSDNeo/AppleSDGothicNeo.ttf") format("truetype");
-    font-weight: bold;
     font-size: 2.9rem;
+    font-weight: bold;
+
     margin-bottom: 4.4rem;
   }
 
   ul {
-    padding-right: 5.8rem;
     display: flex;
     flex-wrap: wrap;
     gap: 2rem 0.9rem;
+    padding-right: 5.8rem;
   }
 
   li {
     font-family: "AppleSDGothicNeoSB00";
     src: url("./AppleSDNeo/AppleSDGothicNeoSB.ttf") format("truetype");
-    font-size: 1.6rem;
     color: ${colors.gray2};
+    font-size: 1.6rem;
     line-height: 2.23rem;
     letter-spacing: -0.032rem;
+
     box-sizing: border-box;
-    padding: 0.6rem 2rem;
     height: 3.6rem;
-    border: 0.1rem solid ${colors.tagGray}; //색 지정 필요.
+    padding: 0.6rem 2rem;
+
+    border: 0.1rem solid ${colors.tagGray};
     border-radius: 1.8rem;
 
     &:hover {
       background-color: ${colors.orange};
+      border-color: ${colors.orange};
       color: ${colors.white};
       cursor: pointer;
-      border-color: ${colors.orange};
     }
   }
 `;
