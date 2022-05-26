9f315abfb8a4f3ad3690c2eccea52e64e04d05a6
Hennization <shy6295@ewhain.net>
2022-05-26 15:55:12 +0900

    feat: image 목데이터 문제 해결

--- src/components/common/ContentsCard/index.jsx
+++ src/components/common/ContentsCard/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BookmarkWrapper, ImgWrapper, StyledRoot, Title } from "./style";
 import { ReactComponent as Mark } from "components/common/assets/icon/bookmark_ic-1.svg";
 import { ReactComponent as UnMark } from "components/common/assets/icon/bookmark_ic.svg";
-import imagee from "../assets/img/fave_card_img1.png";
 
 function ContentsCard({ CardInfo, onClick }) {
   const { id, thumbnail, description, isBookmarked } = CardInfo;
@@ -10,7 +9,7 @@ function ContentsCard({ CardInfo, onClick }) {
   return (
     <StyledRoot>
       <ImgWrapper>
-        <img src={imagee} alt="썸네일" />
+        <img src={thumbnail} alt="썸네일" />
       </ImgWrapper>
       <Title>{description}</Title>.
       <BookmarkWrapper>
