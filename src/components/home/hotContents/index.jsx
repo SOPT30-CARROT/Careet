265eca56657ca33a0f7fc343a3ddc8e2e13150d0
choichoijin <cuj10594@naver.com>
2022-05-27 03:41:19 +0900

    chore: mock data 불러오기까지

--- src/components/home/hotContents/index.jsx
+++ src/components/home/hotContents/index.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyledRoot } from "./style";
+import api from "api/index";
 
 function hotContents() {
-  return <StyledRoot></StyledRoot>;
+  const [hotContent, setHotContent] = useState([]);
+  useEffect(() => {
+    const hotContents = api.mock.fetchHotContents();
+    hotContents.then((res) => setHotContent(res));
+  }, []);
+
+  const hotContentList = hotContent.map((hotContent) => (
+    <li key={hotContent.id}>{hotContent.description}</li>
+  ));
+
+  return (
+    <StyledRoot>
+      <h3>요즘 대세! 인기 콘텐츠</h3>
+      <ul>{hotContentList}</ul>
+    </StyledRoot>
+  );
 }
 
 export default hotContents;
--- src/components/home/hotContents/style.js
+++ src/components/home/hotContents/style.js
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const StyledRoot = styled.div``;
+export const StyledRoot = styled.div`
+  h3 {
+    font-family: "AppleSDGothicNeoB";
+    font-size: 2.9rem;
+    font-weight: bold;
+
+    margin-bottom: 4.4rem;
+  }
+`;
