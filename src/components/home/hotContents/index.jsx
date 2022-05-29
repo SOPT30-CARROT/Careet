d7314f537a8185b43931e85d9d8b0d53ddf79f4a
choichoijin <cuj10594@naver.com>
2022-05-29 17:43:58 +0900

    feat: 레이아웃 구현 및 mock데이터 연결

--- src/components/home/hotContents/index.jsx
+++ src/components/home/hotContents/index.jsx
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { StyledRoot } from "./style";
+import { StyledRoot, StyledContainer } from "./style";
 import api from "api/index";
 
 function hotContents() {
   const [hotContent, setHotContent] = useState([]);
   useEffect(() => {
     const hotContents = api.mock.fetchHotContents();
-    hotContents.then((res) => setHotContent(res));
+    hotContents.then((res) => setHotContent(res.slice(0, 6)));
   }, []);
 
-  const hotContentList = hotContent.map((hotContent) => (
-    <li key={hotContent.id}>{hotContent.description}</li>
+  const hotContentList = hotContent.map((hotContent, index) => (
+    <li key={hotContent.id}>
+      <span>{index + 1}</span>
+      <img src={hotContent.description} />
+      <p>{hotContent.description}</p>
+    </li>
   ));
 
   return (
     <StyledRoot>
-      <h3>요즘 대세! 인기 콘텐츠</h3>
-      <ul>{hotContentList}</ul>
+      <StyledContainer>
+        <h3>요즘 대세! 인기 콘텐츠</h3>
+        <ul>{hotContentList}</ul>
+      </StyledContainer>
     </StyledRoot>
   );
 }
