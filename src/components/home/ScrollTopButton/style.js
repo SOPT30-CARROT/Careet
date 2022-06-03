8baddaed37272074876a7010fcf140183c99695f
choichoijin <cuj10594@naver.com>
2022-06-03 23:35:09 +0900

    css: 스크롤 버튼 위치 조정

--- src/components/home/ScrollTopButton/style.js
+++ src/components/home/ScrollTopButton/style.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 import { ReactComponent as ScrollTopButton } from "components/common/assets/icon/navi_ic.svg";
 
 export const StyledScrollTopButton = styled(ScrollTopButton)`
+  width: 6.7rem;
+  height: 6.7rem;
   position: sticky;
   bottom: 0;
-  margin-left: 100%;
+  margin-left: calc(100vw - 6.7rem);
 `;
